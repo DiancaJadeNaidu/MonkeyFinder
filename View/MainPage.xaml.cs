8fea6f2133ecec3bbc8abe68f0be8bff4e364f07
DiancaJadeNaidu <diancanaidu@gmail.com>
2026-03-03 14:40:25 +0200

    Add project files.

---
+++ View/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonkeyFinder.View
+{
+    internal class MainPage
+    {
+    }
+}
---
+++ ViewModel/MonkeyDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonkeyFinder.ViewModel
+{
+    internal class MonkeyDetailsViewModel
+    {
+    }
+}
